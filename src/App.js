2bca243a113b915a272d979fe334fe57f076a0c6
Sebastian Rotar <rotar.seby1@gmail.com>
2020-03-14 22:16:24 +0100

    fixed path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import DifferentCities from './pages/different-cities/DifferentCities';
 import Home from './pages/home/Home';
 
@@ -12,9 +12,11 @@ export default class App extends Component{
       <div>
         <Header />
         <Switch>
-          <Route path="/different-cities" component={(props) => <DifferentCities {...props} />} />
-          <Route path="/map" component={(props) => <Home {...props} />} />
-          <Route path="/" component={(props) => <Home {...props} />} />
+          <Route path="/different-cities" component={props => <DifferentCities {...props} />} />
+          <Route path="/map" component={props => <Home {...props} />} />
+          <Route exact path="/">
+            <Redirect to="/map" />
+          </Route>
         </Switch>
       </div>
     </Router>
