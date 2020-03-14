8b4b010c28fe7d04d6e3ced16b89cffd9fc4d049
Sebastian Rotar <rotar.seby1@gmail.com>
2020-03-14 19:17:31 +0100

    temp fix

--- src/helpers/forecast.js
+++ src/helpers/forecast.js
@@ -7,8 +7,8 @@ const forecast = async(latitude, longitude, options) => {
     : url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER}&units=metric`;
   try {
     // const proxiedRequest = request.defaults({ proxy: 'http://USER:PASSWORD@myProxy:8080' });
-    //const proxyAdded = 'https://cors-anywhere.herokuapp.com/' + url;
-    const response = await request({ url, json: true, method: 'GET', headers: { 'Access-Control-Allow-Origin': 'https://weatherappwithmap.herokuapp.com/'} });
+    const proxyAdded = 'https://cors-anywhere.herokuapp.com/' + url;
+    const response = await request({ uri: proxyAdded, json: true, method: 'GET', headers: { 'Access-Control-Allow-Origin': 'https://weatherappwithmap.herokuapp.com/'} });
     if(!!options) {
       const customWeatherObject = {};
       customWeatherObject.currently = {...response.currently};
